6d08441d755fc3e59a71e1009ae224db4ab83e19
AlInA84838 <musihinaaliana@yandex.ru>
2024-08-31 16:15:09 +0300

    8-crypto

---
+++ 8-crypto/crypto.js
@@ -0,0 +1,27 @@
+const password = 'qwertyuiop'
+
+function crypto(password) {
+    const p = password.split('');
+    const a = p.slice(0,6);
+    const b = p.slice(6,8);
+    const c = p.slice(8);
+
+    const crP = [].concat(b,a,c).join('')
+    
+    return (crP)
+}
+
+
+const password2 = 'uiqwertyop'
+
+function back(password2) {
+    const e = password2.split(' ');
+    const startPas = (e == crypto(password)) ? true : false;
+    return startPas
+}
+
+
+console.log (crypto(password))
+console.log (back(password2))
+
+
