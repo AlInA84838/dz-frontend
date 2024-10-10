bd5c39845947c994b310e8f88ba81d8d21fabebf
AlInA84838 <musihinaaliana@yandex.ru>
2024-10-10 16:54:15 +0300

    10-array-clean

---
+++ 10-array-clean/10-array-clean.js
@@ -0,0 +1,24 @@
+function removeFunction(num){
+
+    return num % 2 === 0;
+}
+
+
+function filterArray(arr, removeFunction) {
+
+    let result = [];
+    
+   
+    for (let i = 0; i < arr.length; i++) {
+       
+        if (!removeFunction(arr[i])) {
+            result.push(arr[i]);
+        }
+    }
+    
+    return result;
+}
+
+
+const filteredNumbers = filterArray([1, 2, 3, 4, 5, 6], removeFunction)
+console.log(filteredNumbers)
