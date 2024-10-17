bfb323034e1c68487c3ea86887471cabdf1c1d4c
AlInA84838 <musihinaaliana@yandex.ru>
2024-10-17 14:02:52 +0300

    11-date-array

--- 11-date-array/app.js
+++ 11-date-array/app.js
@@ -1,22 +1,54 @@
+
 function newArr(arr) {
-    let endArr = [];
+    let rusArr = [];
+    let engArr = [];
+    let endArray = [];
 
     arr.forEach(date => {
-       
         if (date.includes('.')) {
-            endArr.push(date.split('.').map(Number));
+            rusArr.push(date.split('.').map(Number));
         } else if (date.includes('/')) {
-            endArr.push(date.split('/').map(Number));
+            engArr.push(date.split('/').map(Number));
         }
     });
 
-    endArr = endArr
+    rusArr = rusArr
         .filter(part => part.length === 3)
         .filter(part => (part[0] >= 1 && part[0] <= 31) && (part[1] >= 1 && part[1] <= 12))
         .map(part => part.join('.'));
 
-    return endArr;
+    rusArr = rusArr.filter((date) => {
+        const [day, month, year] = date.split('.').map(Number);
+        if (month === 2 && day === 29) {
+            if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
+                return true;
+            }
+            return false;
+        }
+        return true;
+    });
+
+
+    engArr = engArr
+        .filter(part => part.length === 3)
+        .filter(part => (part[1] >= 1 && part[1] <= 31) && (part[0] >= 1 && part[0] <= 12))
+        .map(part => part.join('/'));
+
+    engArr = engArr.filter((date) => {
+        const [month, day, year] = date.split('/').map(Number);
+        if (month === 2 && day === 29) {
+            if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
+                return true;
+            }
+            return false;
+        }
+        return true;
+    });
+
+
+    endArray = rusArr.concat(engArr);
+
+    return endArray;
 }
 
-console.log(newArr(["10.02.22", "0.13.22", "41.12", "11/12/23"]));
-
+console.log(newArr(["10.02.22", "29.2.23", "41.12", "02/31/23", '2/29/24']));
